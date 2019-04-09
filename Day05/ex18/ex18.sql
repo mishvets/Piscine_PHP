37f22efd740a282265033b805fb4e286a107ebff
mishvets <19mch97@gmail.com>
2019-04-09 20:33:37 +0300

    only two y

--- Day05/ex18/ex18.sql
+++ Day05/ex18/ex18.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM `distrib`
 WHERE `id_distrib` = 42 OR `id_distrib` >= 62 AND `id_distrib` <= 69 OR `id_distrib` = 71 OR `id_distrib` >= 88 AND `id_distrib` <= 90
-OR LOWER(`name`) LIKE '%y%y%'
+OR (length(`name`) - length(REPLACE(LOWER(`name`), 'y', ''))) = 2
 LIMIT 2, 5;
